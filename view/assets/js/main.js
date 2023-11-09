76a0188733bf5899850eb9215c1d2242d1d1ab26
unknown <kousay.najar@esprit.tn>
2023-11-09 10:07:06 +0100

    msg d'erreur et css

--- view/assets/js/main.js
+++ view/assets/js/main.js
@@ -293,3 +293,32 @@
   new PureCounter();
 
 })();
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
+function myFunction1() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("mySearch");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myMenu");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+      }
+    }
+    
+}
+//---------------------------------------------------------------------------------------------
+/*function vald()
+{
+  
+}*/
+    
